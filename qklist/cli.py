420b43b7fa4629a19d0757e3be0480bfb7c5379a
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-08-02 01:04:36 -0400

    Prepare database file and init setup

--- qklist/cli.py
+++ qklist/cli.py
@@ -1,10 +1,38 @@
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from qklist import __app_name__, __version__
+from qklist import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        "--db-path",
+        "-db",
+        prompt="Enter qklist database location. Type a path or press enter to confirm default",
+    ),
+) -> None:
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating qklist configuration file failed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating qklist database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"Tbe database for qklist saved as [{db_path}]", fg=typer.colors.GREEN)
+
 
 def _version_callback(value: bool) -> None:
     if value:
