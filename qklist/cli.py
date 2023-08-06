8979228f563922c90fa27be9e648c655470d66c9
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-08-06 00:27:13 -0400

    Update add function in cli and add list function to cli

--- qklist/cli.py
+++ qklist/cli.py
@@ -41,19 +41,69 @@ def get_qklist() -> qklist.QkListObj:
         db_path = database.get_database_path(config.CONFIG_FILE_PATH)
     else:
         typer.secho(
-            'Config file not found. Please, run "qklist init"',
-            fg=typer.colors.RED,
-        )
+                'Config file not found. Please, run "qklist init"',
+                fg=typer.colors.RED,
+                )
         raise typer.Exit(1)
     if db_path.exists():
         return qklist.QkListObj(db_path)
     else:
         typer.secho(
-            'Database not found. Please, run "qklist init"',
-            fg=typer.colors.RED,
-        )
+                'Database not found. Please, run "qklist init"',
+                fg=typer.colors.RED,
+                )
         raise typer.Exit(1)
 
+@app.command()
+def add(
+        description: List[str] = typer.Argument(...),
+        priority: int = typer.Option(2, "--priority", "-p", min=1, max=3),
+        ) -> None:
+    """Add a new item with a DESCRIPTION."""
+    current_qklist = get_qklist()
+    qklist_item, error = current_qklist.add(description, priority)
+    if error:
+        typer.secho(
+                f'Adding item failed with "{ERRORS[error]}"', fg=typer.colors.RED
+                )
+        raise typer.Exit(1)
+    else:
+        typer.secho(
+                f"""item: "{qklist_item['Description']}" was added """
+                f"""with priority: {priority}""",
+                fg=typer.colors.GREEN,
+                )
+
+@app.command(name="list")
+def list_all() -> None:
+    """List all quick list items."""
+    current_qklist = get_qklist()
+    qklist_items = current_qklist.get_qklist_items()
+    if len(qklist_items) == 0:
+        typer.secho(
+            "There are no tasks in quick list", fg=typer.colors.RED
+        )
+        raise typer.Exit()
+    typer.secho("\nitem list:\n", fg=typer.colors.BLUE, bold=True)
+    columns = (
+        "ID.  ",
+        "| Priority  ",
+        "| Done  ",
+        "| Description  ",
+    )
+    headers = "".join(columns)
+    typer.secho(headers, fg=typer.colors.BLUE, bold=True)
+    typer.secho("-" * len(headers), fg=typer.colors.BLUE)
+    for id, list_item in enumerate(qklist_items, 1):
+        desc, priority, done = list_item.values()
+        typer.secho(
+            f"{id}{(len(columns[0]) - len(str(id))) * ' '}"
+            f"| ({priority}){(len(columns[1]) - len(str(priority)) - 4) * ' '}"
+            f"| {done}{(len(columns[2]) - len(str(done)) - 2) * ' '}"
+            f"| {desc}",
+            fg=typer.colors.BLUE,
+        )
+    typer.secho("-" * len(headers) + "\n", fg=typer.colors.BLUE)
 
 def _version_callback(value: bool) -> None:
     if value:
